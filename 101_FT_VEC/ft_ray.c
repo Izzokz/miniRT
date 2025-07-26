8616d11eddf4d32b4a503010da60880a6f787b00
Acholias <mugot.lucas@gmail.com>
2025-07-26 15:43:08 +0200

    Normed vector's fonctionq

--- 101_FT_VEC/ft_ray.c
+++ 101_FT_VEC/ft_ray.c
@@ -6,7 +6,7 @@
 /*   By: lumugot <lumugot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 17:40:35 by kzhen-cl          #+#    #+#             */
-/*   Updated: 2025/07/26 13:04:12 by lumugot          ###   ########.fr       */
+/*   Updated: 2025/07/26 15:42:22 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	ft_ray_dir(t_ray ray, const t_vec target)
 {
-	ft_vec_sub(ray[1], target, ray[0]);
-	ft_vec_norm(ray[1], ray[1]);
+	ft_vec_sub(*(ray + 1), target, *ray);
+	ft_vec_norm(*(ray + 1), *(ray + 1));
 }
 
 void	ft_new_ray(t_ray newr, const t_vec pos, const t_vec target)
 {
-	ft_cpy_vec(newr[0], pos);
+	ft_cpy_vec(*newr, pos);
 	ft_ray_dir(newr, target);
 }
 
 void	ft_cpy_ray(t_ray newr, const t_ray cpy)
 {
-	ft_cpy_vec(newr[0], cpy[0]);
-	ft_cpy_vec(newr[1], cpy[1]);
+	ft_cpy_vec(*newr, *cpy);
+	ft_cpy_vec(*(newr + 1), *(cpy + 1));
 }
