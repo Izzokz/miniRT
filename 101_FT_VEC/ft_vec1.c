f9480dad93b2cc114953e687a08e5cd417ac59c4
Acholias <mugot.lucas@gmail.com>
2025-07-26 13:05:07 +0200

    Change all vec fonctions without fucking *

--- 101_FT_VEC/ft_vec1.c
+++ 101_FT_VEC/ft_vec1.c
@@ -6,7 +6,7 @@
 /*   By: lumugot <lumugot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:34:31 by kzhen-cl          #+#    #+#             */
-/*   Updated: 2025/06/19 13:14:55 by lumugot          ###   ########.fr       */
+/*   Updated: 2025/07/26 13:04:05 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 inline double	ft_vec_dot(const t_vec v1, const t_vec v2)
 {
-	return ((*v1 * *v2) + (*(v1 + 1) * *(v2 + 1)) + (*(v1 + 2) * *(v2 + 2)));
+	return (v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]);
 }
 
 inline double	ft_vec_mag(const t_vec v)
@@ -34,12 +34,12 @@ void	ft_vec_norm(t_vec newv, const t_vec v)
 
 void	ft_vec_neg(t_vec newv, const t_vec v)
 {
-	ft_new_vec(newv, -*v, -*(v + 1), -*(v + 2));
+	ft_new_vec(newv, -v[0], -v[1], -v[2]);
 }
 
 inline double	ft_vec_dist(const t_vec v1, const t_vec v2)
 {
-	return (sqrt(((*v2 - *v1) * (*v2 - *v1))
-			+ ((*(v2 + 1) - *(v1 + 1)) * (*(v2 + 1) - *(v1 + 1)))
-			+ ((*(v2 + 2) - *(v1 + 2)) * ((*(v2 + 2) - *(v1 + 2))))));
+	return (sqrt(((v2[0] - v1[0]) * (v2[0] - v1[0]))
+			+ ((v2[1] - v1[1]) * (v2[1] - v1[1]))
+			+ ((v2[2] - v1[2]) * (v2[2] - v1[2]))));
 }
