f2b3a1f5fa43d59c6f830662b16388d0c34723d2
Acholias <mugot.lucas@gmail.com>
2025-07-27 16:00:45 +0200

    Norme ft_init_cam_vector (split and rebuild) :)

--- 103_FT_PROCESS/ft_process.c
+++ 103_FT_PROCESS/ft_process.c
@@ -6,7 +6,7 @@
 /*   By: lumugot <lumugot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 13:17:22 by lumugot           #+#    #+#             */
-/*   Updated: 2025/07/27 14:32:07 by lumugot          ###   ########.fr       */
+/*   Updated: 2025/07/27 16:00:10 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,45 +83,60 @@ static inline void	ft_put_color(t_mlx_obj *mobj,
 	*(unsigned int *)dst = color;
 }
 
-static void	ft_init_cam_vectors(t_vec cam_vectors[3], const t_scene *s)
+static void	ft_init_up_vec(t_vec up, const t_vec forward)
 {
-    double	win_ratio;
-    double	plane_h;
-    double	plane_w;
-    t_vec	cam_d;
-    t_vec	forward;
-    t_vec	right;
-    t_vec	up;
-    double	cr, sr;
-    t_vec	right_rot;
+	if (fabs(forward[1]) > 0.99)
+		ft_new_vec(up, 0, 0, 1);
+	else
+		ft_new_vec(up, 0, 1, 0);
+}
+
+static void	ft_init_right_up(
+	t_vec right, t_vec up, const t_vec forward)
+{
+	ft_vec_cross(right, up, forward);
+	ft_vec_norm(right, right);
+	ft_vec_cross(up, forward, right);
+	ft_vec_norm(up, up);
+}
+
+static void	ft_apply_roll(
+	t_vec right, t_vec up, double roll)
+{
+	double	cr;
+	double	sr;
+	t_vec	right_rot;
 	t_vec	up_rot;
 
-    win_ratio = (double)WIDTH / (double)HEIGHT;
-    plane_h = tan(s->camera.fov * 0.5 * (PI / 180.0)) * 2.0;
-    plane_w = plane_h * win_ratio;
-    ft_cpy_vec(forward, s->_forward);
-    if (fabs(forward[1]) > 0.99)
-        ft_new_vec(up, 0, 0, 1);
-    else
-        ft_new_vec(up, 0, 1, 0);
-    ft_vec_cross(right, up, forward);
-    ft_vec_norm(right, right);
-    ft_vec_cross(up, forward, right);
-    ft_vec_norm(up, up);
-    cr = cos(s->_roll);
-    sr = sin(s->_roll);
-    right_rot[0] = cr * right[0] - sr * up[0];
-    right_rot[1] = cr * right[1] - sr * up[1];
-    right_rot[2] = cr * right[2] - sr * up[2];
-    up_rot[0] = sr * right[0] + cr * up[0];
-    up_rot[1] = sr * right[1] + cr * up[1];
-    up_rot[2] = sr * right[2] + cr * up[2];
-    ft_cpy_vec(right, right_rot);
-    ft_cpy_vec(up, up_rot);
-    ft_vec_scale(cam_vectors[0], right, plane_w);
-    ft_vec_scale(cam_vectors[1], up, plane_h);
-    ft_vec_scale(cam_d, forward, 2.0);
-    ft_vec_sub(cam_vectors[2], s->camera.pos, cam_d);
+	cr = cos(roll);
+	sr = sin(roll);
+	right_rot[0] = cr * right[0] - sr * up[0];
+	right_rot[1] = cr * right[1] - sr * up[1];
+	right_rot[2] = cr * right[2] - sr * up[2];
+	up_rot[0] = sr * right[0] + cr * up[0];
+	up_rot[1] = sr * right[1] + cr * up[1];
+	up_rot[2] = sr * right[2] + cr * up[2];
+	ft_cpy_vec(right, right_rot);
+	ft_cpy_vec(up, up_rot);
+}
+
+static void	ft_init_cam_vectors(t_vec cam_vectors[3], const t_scene *s)
+{
+	double	win_ratio;
+	double	plane_h;
+	t_vec	tmp1;
+	t_vec	tmp2;
+
+	win_ratio = (double)WIDTH / (double)HEIGHT;
+	plane_h = tan(s->camera.fov * 0.5 * (PI / 180.0)) * 2.0;
+	ft_cpy_vec(tmp1, s->_forward);
+	ft_init_up_vec(tmp2, tmp1);
+	ft_init_right_up(cam_vectors[0], tmp2, tmp1);
+	ft_apply_roll(cam_vectors[0], tmp2, s->_roll);
+	ft_vec_scale(cam_vectors[0], cam_vectors[0], plane_h * win_ratio);
+	ft_vec_scale(cam_vectors[1], tmp2, plane_h);
+	ft_vec_scale(tmp1, s->_forward, 2.0);
+	ft_vec_sub(cam_vectors[2], s->camera.pos, tmp1);
 }
 
 static inline void	ft_hit_parse(t_mlx_obj *mobj, int i[2],
@@ -132,7 +147,7 @@ static inline void	ft_hit_parse(t_mlx_obj *mobj, int i[2],
 	hit = ft_hit_nearest_obj(s->ray, s->objects);
 	if (hit)
 		return (ft_put_color(mobj, *i, *(i + 1),
-			r->coloration(s->ray, hit, s, r)));
+				r->coloration(s->ray, hit, s, r)));
 	if (r->coloration == ft_unicorn)
 		return (ft_put_color(mobj, *i, *(i + 1), 0xdc96ff));
 	if (r->coloration == ft_color_virus)
@@ -141,7 +156,7 @@ static inline void	ft_hit_parse(t_mlx_obj *mobj, int i[2],
 		return (ft_put_color(mobj, *i, *(i + 1), 0xcffff9));
 	if (r->coloration == ft_color_error)
 		return (ft_put_color(mobj, *i, *(i + 1),
-			0xff0000 + 255 * (rand() % 2)));
+				0xff0000 + 255 * (rand() % 2)));
 	ft_put_color(mobj, *i, *(i + 1), 0);
 }
 
