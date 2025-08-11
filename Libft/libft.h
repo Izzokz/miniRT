8a4a6ee555f94639de85f367cbaaea028fa0347e
Izzokz <askilola@hotmail.fr>
2025-08-11 19:14:02 +0200

    Fixed header protection

--- Libft/libft.h
+++ Libft/libft.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-# define LIBFT_BONUS_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include <unistd.h>
 # include <stdlib.h>
