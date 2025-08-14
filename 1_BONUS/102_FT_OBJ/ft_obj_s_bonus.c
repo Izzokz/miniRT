ccb6e78d860a058c466cee699d0a8886b51f01c3
Izzokz <askilola@hotmail.fr>
2025-08-14 04:26:12 +0200

    [NEW] circles inside is now handled
    Handling : ft_hit_s returns 0 (as not hit)

--- 1_BONUS/102_FT_OBJ/ft_obj_s_bonus.c
+++ 1_BONUS/102_FT_OBJ/ft_obj_s_bonus.c
@@ -17,27 +17,39 @@ void	ft_reflect(t_ray ray, t_vec posnorm[2])
 	double	dot;
 	t_vec	tmp;
 
-	ft_vec_scale(tmp, ray[1], 1e-3);
-	ft_vec_add(ray[0], ray[0], tmp);
 	dot = 2.0 * ft_vec_dot(ray[1], posnorm[1]);
 	ft_vec_scale(tmp, posnorm[1], dot);
 	ft_cpy_vec(ray[0], posnorm[0]);
 	ft_vec_sub(ray[1], ray[1], tmp);
 	ft_vec_norm(ray[1], ray[1]);
+	ft_vec_scale(tmp, ray[1], 1e-3);
+	ft_vec_add(ray[0], ray[0], tmp);
+}
+
+static inline char	ft_set_sphere(const t_obj *sphere, const t_ray ray,
+	double abdt[4], t_vec tmp[2])
+{
+	char	inside;
+	double	dot;
+	double	r2;
+
+	ft_vec_sub(*tmp, *ray, sphere->params);
+	dot = ft_vec_dot(*tmp, *tmp);
+	r2 = (*(sphere->params + 3) / 2) * (*(sphere->params + 3) / 2);
+	inside = dot < r2;
+	*abdt = ft_vec_dot(ray[1], ray[1]);
+	abdt[1] = 2.0 * ft_vec_dot(tmp[0], ray[1]);
+	abdt[2] = (abdt[1] * abdt[1]) - (4.0 * abdt[0] * (dot - r2));
+	return (inside);
 }
 
 char	ft_hit_s(const t_obj *sphere, t_ray ray)
 {
-	double	abdt[4];
-	t_vec	tmp[2];
+	double		abdt[4];
+	t_vec		tmp[2];
+	const char	inside = ft_set_sphere(sphere, ray, abdt, tmp);
 
-	ft_vec_sub(tmp[0], ray[0], sphere->params);
-	abdt[0] = ft_vec_dot(ray[1], ray[1]);
-	abdt[1] = 2.0 * ft_vec_dot(tmp[0], ray[1]);
-	abdt[2] = (abdt[1] * abdt[1])
-		- (4.0 * abdt[0] * (ft_vec_dot(tmp[0], tmp[0])
-				- ((*(sphere->params + 3) / 2) * (*(sphere->params + 3) / 2))));
-	if (abdt[2] < 1e-6)
+	if (inside || abdt[2] < 1e-6)
 		return (0);
 	abdt[3] = (-abdt[1] - sqrt(abdt[2])) / (2.0 * abdt[0]);
 	if (abdt[3] < 1e-6)
@@ -48,6 +60,8 @@ char	ft_hit_s(const t_obj *sphere, t_ray ray)
 	ft_vec_add(tmp[0], ray[0], tmp[0]);
 	ft_vec_sub(tmp[1], tmp[0], sphere->params);
 	ft_vec_norm(tmp[1], tmp[1]);
+	if (inside)
+		ft_vec_scale(tmp[1], tmp[1], -1);
 	ft_reflect(ray, tmp);
 	return (1);
 }
